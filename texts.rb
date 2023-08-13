e6375a88806d2f726059942c07ea83585c63ae08
Felipe <felipe.baleche2@gmail.com>
2023-08-13 18:28:01 -0300

    fixes

--- texts.rb
+++ texts.rb
@@ -6,8 +6,7 @@ class Text
         puts "Choose the game mode:"
         puts "Press '1' to be the code MAKER."
         puts "Press '2' to be the code BREAKER."
-        x = gets
-        p x.class
+        
     end
 
     def clear
