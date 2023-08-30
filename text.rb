1c2b2f7a84136aba631774cd56cbaa952a8e9a42
Felipe <felipe.baleche2@gmail.com>
2023-08-30 17:37:44 -0300

    fixed order of methods

--- text.rb
+++ text.rb
@@ -2,16 +2,16 @@ require 'os'
 
 class Text
 
+    def self.clear
+        system(OS.windows? ? "cls" : "clear")
+    end 
+
     def self.menu
         puts "Welcome to Master Mind!"
         puts "Choose the game mode:"
         puts "Press '1' to be the code MAKER."
         puts "Press '2' to be the code BREAKER."        
     end
-
-    def self.clear
-        system(OS.windows? ? "cls" : "clear")
-    end 
 
     def self.code_breaker_win_end
         puts "You won!"
